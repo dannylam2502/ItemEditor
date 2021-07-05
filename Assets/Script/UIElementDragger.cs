5ef4aff4289e04728767c6cdac801ae43879a283
tklqd2502 <dlam@subdreamstudios.com>
2021-07-04 22:16:14 -0700

    - update mouse sensitivity

--- Assets/Script/UIElementDragger.cs
+++ Assets/Script/UIElementDragger.cs
@@ -4,12 +4,13 @@ using UnityEngine.UI;
 
 public class UIElementDragger : MonoBehaviour, IDragHandler
 {
+    public const float SENSIVITY = 0.1f;
 
     private bool dragging;
 
     public void OnDrag(PointerEventData eventData)
     {
-        this.transform.position += (Vector3)eventData.delta * 0.01f;
+        this.transform.position += (Vector3)eventData.delta * SENSIVITY;
     }
 
     public void Update()
